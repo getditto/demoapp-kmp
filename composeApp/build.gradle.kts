0a1b40bcddd230d39c26f822e68d3e47b698ab95
Ben Chatelain <ben@octop.ad>
2024-04-30 13:51:06 -0600

    ⚰️ Remove dead code

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -17,16 +17,6 @@ kotlin {
         }
     }
 
-//    listOf(
-//        iosArm64(),
-//        iosSimulatorArm64()
-//    ).forEach { iosTarget ->
-//        iosTarget.binaries.framework {
-//            baseName = "ComposeApp"
-//            isStatic = true
-//        }
-//    }
-
     iosArm64()
     iosSimulatorArm64()
 
