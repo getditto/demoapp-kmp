35e4047924c48a5556979409735fb789ab342bea
Ben Chatelain <ben@octop.ad>
2024-05-27 17:08:15 -0600

    ✅ Use kotlin.test assertion

--- composeApp/src/androidInstrumentedTest/kotlin/live/ditto/demo/kotlinmultipeer/AndroidGreetingTest.kt
+++ composeApp/src/androidInstrumentedTest/kotlin/live/ditto/demo/kotlinmultipeer/AndroidGreetingTest.kt
@@ -1,12 +1,12 @@
 package live.ditto.demo.kotlinmultipeer
 
 import Greeting
-import junit.framework.Assert.assertTrue
 import org.junit.Test
+import kotlin.test.assertTrue
 
 class AndroidGreetingTest {
     @Test
     fun testExample() {
-        assertTrue("Check Android is mentioned", Greeting().greet().contains("Android"))
+        assertTrue(Greeting().greet().contains("Android"))
     }
 }
