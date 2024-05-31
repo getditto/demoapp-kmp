18eb4117f974a6eabb8c66ba6fa54626d9671a68
Ben Chatelain <ben@octop.ad>
2024-05-31 08:26:21 -0600

    🤡 Add MockDitto to fix tests

--- composeApp/src/androidInstrumentedTest/kotlin/live/ditto/demo/kotlinmultipeer/AndroidGreetingTest.kt
+++ composeApp/src/androidInstrumentedTest/kotlin/live/ditto/demo/kotlinmultipeer/AndroidGreetingTest.kt
@@ -1,15 +1,8 @@
 package live.ditto.demo.kotlinmultipeer
 
-import DittoManager
 import Greeting
 import org.junit.Test
 import kotlin.test.assertTrue
-
-object MockDitto : DittoManager() {
-    override val version = "0.0.0"
-
-    override fun startSync() { /* no-op */ }
-}
 
 class AndroidGreetingTest {
     @Test
--- composeApp/src/androidUnitTest/kotlin/live/ditto/demo/kotlinmultipeer/AndroidGreetingTest.kt
+++ composeApp/src/androidUnitTest/kotlin/live/ditto/demo/kotlinmultipeer/AndroidGreetingTest.kt
@@ -1,15 +1,8 @@
 package live.ditto.demo.kotlinmultipeer
 
-import DittoManager
 import Greeting
 import org.junit.Assert.assertTrue
 import org.junit.Test
-
-object MockDitto : DittoManager() {
-    override val version = "0.0.0"
-
-    override fun startSync() { /* no-op */ }
-}
 
 class AndroidGreetingTest {
     @Test
