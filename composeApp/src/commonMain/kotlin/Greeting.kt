40e5b8bddff41ef87fd13a2846cb6bafe60102af
Ben Chatelain <ben@octop.ad>
2024-06-02 21:13:15 -0600

    💬 Update greeting text

--- composeApp/src/commonMain/kotlin/Greeting.kt
+++ composeApp/src/commonMain/kotlin/Greeting.kt
@@ -4,6 +4,6 @@ class Greeting(
     private val platform: Platform = getPlatform()
 
     fun greet(): String {
-        return "Hello, ${platform.name}! \nDitto ${dittoManager.version}"
+        return "Running on ${platform.name} \nDitto ${dittoManager.version}"
     }
 }
